31e00646642a283e71904c6c6d0de868278bb3c3
jasn-armstrng <jasnarmstrng@gmail.com>
2025-02-05 01:26:31 +0000

    docs: Explain reason for strcspn use

--- app/main.c
+++ app/main.c
@@ -8,11 +8,17 @@ int main() {
   // Uncomment this block to pass the first stage
   printf("$ ");
 
-
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = '\0';
+
+  // Note: fgets() Captures the newline (\n). This would cause the output to be printed as,
+  //       "invalid command:"
+  //       "command not found"
+  // So we remove the trailing newline character if present and replace with a null terminator.
+  input[strcspn(input, "\n")] = '\0';  // lhs: strcspn returns the index position of "\n" in the string if
+                                                  // found. rhs: assign that position the value '\0'. strcspn is provi-
+                                                  // ded by <string.h>.
   printf("%s: command not found", input);
 
   return 0;
